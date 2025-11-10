11ba97d37edd6cd565fab0c2f1b20cddc6c455e0
Nicolas Baya-Casal Sansolini <nbaysan0711@ieszaidinvergeles.org>
2025-11-10 09:35:30 +0100

    updated variables on shortpath.py

--- shortpath.py
+++ shortpath.py
@@ -5,7 +5,7 @@ def open_short_url(short_code, base_domain):
     fqdn = f"{short_code}.{base_domain}"
     answers = dns.resolver.resolve(fqdn, 'TXT')
     for rdata in answers:
-        full_url = ''.join(rdata.strings).strip('"')
+        full_url = ''.join(s.decode() for s in rdata.strings).strip('"')
         webbrowser.open(full_url)
         return
 
